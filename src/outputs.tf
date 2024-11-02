d4df024fad2cea6031c1b95bbf2d076c041e0a7f
Igor Rodionov <goruha@gmail.com>
2024-11-02 19:22:25 +0100

    Initial commit

--- src/outputs.tf
+++ src/outputs.tf
@@ -1,4 +1,9 @@
-output "mock" {
-  description = "Mock output example for the Cloud Posse Terraform component template"
-  value       = local.enabled ? "hello ${basename(abspath(path.module))}" : ""
+output "instance_id" {
+  value       = module.ec2_zscaler.*.id
+  description = "Instance ID"
+}
+
+output "private_ip" {
+  value       = module.ec2_zscaler.*.private_ip
+  description = "Private IP of the instance"
 }
--- src/versions.tf
+++ src/versions.tf
@@ -1,5 +1,26 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 0.13.0"
 
-  required_providers {}
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0"
+    }
+    template = {
+      source  = "cloudposse/template"
+      version = ">= 2.2"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    utils = {
+      source  = "cloudposse/utils"
+      version = ">= 1.10.0"
+    }
+  }
 }
